2a49ca55661cfbb962dbbd7d3297b86c3c1b9287
Jose Rodriguez Ospina <josem.rodriguezo@uqvirtual.edu.co>
2024-11-10 19:06:31 -0500

    a

---
+++ src/main/java/co/edu/uniquindio/poo/viewController/InicioViewController.java
@@ -0,0 +1,11 @@
+package co.edu.uniquindio.poo.viewController;
+
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+
+public class InicioViewController {
+    @FXML
+    private ResourceBundle resources;
+
+}
